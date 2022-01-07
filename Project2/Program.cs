1d5b3707f7631b3fd9fc7060bb312b8c3327b3bd
Avart <Avart@AVART->
2022-01-07 13:40:56 +0300

    Добавьте файлы проекта.

---
+++ Project2/Program.cs
@@ -0,0 +1,24 @@
+﻿int rndm;
+int ans, cnt = 0;
+Random rnd = new Random();
+do
+{
+	rndm = rnd.Next(0, 101);
+	ans = Convert.ToInt32(Console.ReadLine());
+	if (ans == rndm)
+	{
+		Console.WriteLine("Верно");
+		Console.WriteLine($"{cnt} попыток");
+	}
+	else if (ans > rndm)
+	{
+		Console.WriteLine($"{ans} больше чем заданное число");
+		cnt += 1;	
+	}
+	else if (ans < rndm)
+	{
+		Console.WriteLine($"{ans} меньше чем заданное число");
+		cnt += 1;
+	}
+}
+while (ans != rndm);
